e7d498ae75ce9dbafd9f5f84f41e5111e2cc2516
Emily Draper <emilymedraper@gmail.com>
2020-11-11 13:35:04 -0800

    emily

---
+++ analysis/Hugh/Scripts/.ipynb_checkpoints/project_functions-checkpoint.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from numpy import nan as NA
+
+def load_and_process(dataset):
+    df1 = (
+        pd.read_csv(dataset)
+        .dropna()
+        .rename(columns={'dteday':'Date','yr':'Year','cnt':'Total'})
+
+    )
+    
+    df2 = (
+        df1.drop(columns=['instant'])
+            .assign(Ratio = df1['casual']/df1['registered'])
+    )
+    
+    return df2
