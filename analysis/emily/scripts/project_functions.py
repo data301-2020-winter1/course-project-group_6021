f7c093058ae6854b48b7ba27fc719deb337aaa3d
Emily Draper <emilymedraper@gmail.com>
2020-11-14 08:34:31 -0800

    emily

--- analysis/emily/scripts/project_functions.py
+++ analysis/emily/scripts/project_functions.py
@@ -1,3 +1,5 @@
+#Created a module
+
 import pandas as pd
 import numpy as np
 from numpy import nan as NA
@@ -6,12 +8,33 @@ def load_and_process(dataset):
     df1 = (
         pd.read_csv(dataset)
         .dropna()
-        .rename(columns={'dteday':'date','yr':'year', 'mnth': 'month', 'weathersit': 'weather', 'temp':'temprature', 'hum':'humidity', 'casual': 'casual_users', 'registered': 'registered_users','cnt':'Total'})
+        .rename(columns={'dteday':'date','yr':'year','mnth': 'month','weathersit': 'weather','temp':'temprature','hum':'humidity','cnt':'Total'})
 
     )
     
     df2 = (
         df1.drop(columns=['instant', 'atemp'])
+            .assign(Ratio = df1['casual']/df1['registered'])
+    )
+    
+    return df2
+
+
+
+import pandas as pd
+import numpy as np
+from numpy import nan as NA
+
+def load_and_process(dataset):
+    df1 = (
+        pd.read_csv(dataset)
+        .dropna()
+        .rename(columns={'dteday':'Date','yr':'Year','cnt':'Total'})
+        
+    )
+    
+    df2 = (
+        df1.drop(columns=['instant'])
             .assign(Ratio = df1['casual']/df1['registered'])
     )
     
